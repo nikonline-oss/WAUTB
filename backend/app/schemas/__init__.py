9aa5d5ce33c19d75a11dfcb9532f268a03be447b
nikonline-oss <nikolajserbatyh185@gmail.com>
2025-10-18 09:15:20 +0300

    удалил схемы

--- backend/app/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from .TableTemplate import TableTemplateUpdate,TableTemplateResponse,TableTemplateCreate
-from .paginate import SortCondition, PaginatedResponse,PaginatedRequest,FilterCondition
-from .TableColumn import TableColumnCreate, TableColumnResponse,TableColumnUpdate
-from .TableRecord import TableRecordCreate, TableRecordResponse ,TableRecordUpdate
-from .User import UserCreate, UserResponse, UserUpdate, UserResponseWithTemplates
